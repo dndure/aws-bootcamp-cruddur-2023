dd6ecc8263cecb66b64fb8b15931e12c311c80e9
dndure <d.ndure@gmail.com>
2023-06-09 12:29:37 +0000

    we have a query working

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,12 +1,13 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 
-from lib.db import pool
+from lib.db import pool, query_wrap_object, query_wrap_array
 
 tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run(cognito_user_id=None):
+    print("HOME ACTIVITY")
     #logger.info("HomeActivities")
     with tracer.start_as_current_span("home-activites-mock-data"):
       span = trace.get_current_span()
